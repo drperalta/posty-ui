e2f96bd29891b2068ff5d8f0c6a92423d9756ce7
David Peralta <peraltadavidr@outlook.com>
2023-01-31 21:31:27 +0800

    fetch user details on mounted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { useAuthStore } from "./common/store/auth";
+import { useUserStore } from "./common/store/user";
 
 const authStore = useAuthStore();
+const userStore = useUserStore();
 
 onMounted(() => {
   const token = localStorage.getItem("token");
 
   if (token) {
     authStore.token = token;
+
+    // Get logged in user details
+    userStore.getUserDetails();
   }
 
   console.log(authStore.token);
