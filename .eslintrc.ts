7d677aa8e8e225f79ec69c324f43fa255231f381
David Peralta <peraltadavidr@outlook.com>
2023-01-31 16:05:13 +0800

    add eslint & prettier

---
+++ .eslintrc.ts
@@ -0,0 +1,11 @@
+export default {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "vue/require-default-prop": "off",
+  },
+};
